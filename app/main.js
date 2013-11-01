af713f4dd875326d5c66a1ae3caf88de89c5d59f
soezen <rogge.suzan@gmail.com>
2013-11-01 16:53:48 +0100

    fix port

--- app/main.js
+++ app/main.js
@@ -3,5 +3,5 @@ var server = require('./node_modules/server');
 var router = require('./node_modules/router');
 var handlers = require('./node_modules/handlers');
 
-server.start(router.route, handlers.handle);
+server.start(router.route, handlers.handle, process.env.PORT);
 
